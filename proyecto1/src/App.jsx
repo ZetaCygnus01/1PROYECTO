cb60b79e77dd6345c43dc468fc0a0d95eea3c216
ZetaCygnus01 <122745586+ZetaCygnus01@users.noreply.github.com>
2026-09-16 21:17:34 +0000

    Redireccionando formulario

--- proyecto1/src/App.jsx
+++ proyecto1/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Inicio from "./Pages/Inicio";
 import Prueba from "./Pages/Prueba";
+import Formulario from "./Pages/Formulario"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
         <Route path="/" element={<Inicio />} />
         <Route path="/prueba" element={<Prueba />} />
+        <Route path="/formulario" element={<Formulario />} />
 
       </Routes>
 
--- proyecto1/src/Pages/Formulario.jsx
+++ proyecto1/src/Pages/Formulario.jsx
@@ -1,12 +1,17 @@
 import Label from "../componentes/Label";
 import Input from "../componentes/Input";
 import Boton from "../componentes/Boton";
+import { Link } from "react-router-dom";
 
 function Formulario() {
     return (
         <div>
             <h1>Formulario</h1>
-
+                <Link to="/">
+                    Volver al inicio
+                </Link>
+            <br />
+            <br />
             <Label texto="Nombre" />
 
             <Input
